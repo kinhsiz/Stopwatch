cf46ae0e8c68d54de6b952ad03719ab06c1e893f
Ruben Rios <rriosal@protonmail.com>
2021-08-02 18:13:38 -0500

    make changes

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    let seconds = 00;
+    let tens = 00;
+    let appendTens = dcoument.getElementById("tens");
+    let appendSeconds = docuent.getElementById("seconds");
+    let buttonStart = document.getElementById("button-start");
+    let buttonStop = document.getElementById("button-stop");
+    let buttonReset = document.getElementById("button-reset");
+    let interval;
+
+    )
+}
